c324e63c44ec1611ffed8ee884681288a5c087d0
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-03-29 04:57:24 -0300

    feat: ensure user can search for barbershops

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -1,16 +1,58 @@
 "use client"
 
 import { Button } from "@/app/_components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/app/_components/ui/form";
 import { Input } from "@/app/_components/ui/input";
 import { SearchIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
 
-const Search = () => {
+import { z } from "zod"
+import { useRouter } from "next/navigation";
+ 
+const formSchema = z.object({
+  search: z.string({
+    required_error: "Campo Obrigatório.",
+  }).trim().min(1, "Campo Obrigatório.")
+})
+
+interface SearchProps {
+    defaultValues?: z.infer<typeof formSchema>;
+}
+
+const Search = ({defaultValues}: SearchProps) => {
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues,
+    });
+
+    const handleSubmit = (data: z.infer<typeof formSchema>) => {
+        router.push(`/barbershops?search=${data.search}`);
+    }   
+
     return (
         <div className="flex items-center gap-2">
-            <Input placeholder="Busque por uma barbearia..." />
-            <Button variant="default">
-                <SearchIcon size={20} />
-            </Button>
+            <Form {...form}>
+                <form className="flex gap-4 w-full" onSubmit={form.handleSubmit(handleSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="search"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <Input placeholder="Busque por uma barbearia..." {...field} />
+                                </FormControl>                            
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button variant="default" type="submit">
+                        <SearchIcon size={20} />
+                    </Button>
+                </form>
+            </Form>
         </div>
     );
 }
